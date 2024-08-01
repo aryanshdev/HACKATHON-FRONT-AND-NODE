e7a9344df15959840e69a9974e730699669066e8
Pratyush Birole <birole.pratyush@gmail.com>
2024-08-01 11:27:40 +0530

    Routing

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/useComp/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TrainModel } from './components/useComp/TrainModel';
+import RealMain from './components/useComp/RealMain';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RealMain/>} />
+        <Route path="/trainmodel" element={<TrainModel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
