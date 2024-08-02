a9d81a892a88eae33db420b33f872b6ef2545f55
Prakhar <prakharswarnkar29@gmail.com>
2024-08-02 14:16:00 +0530

    Models INputs Changed

--- Frontend/src/components/useComp/RunModels.jsx
+++ Frontend/src/components/useComp/RunModels.jsx
@@ -46,16 +46,25 @@ const RunModels = () => {
 
           {/* Model Training */}
           <br />
-          <h1 style={{ fontSize: "30px", fontWeight: "bolder" }}> <i className="fa-solid fa-file-zipper" style={{ color: "#036EFD" }}></i> &nbsp; Model Training</h1><br/><br/>
+          <h1 style={{ fontSize: "30px", fontWeight: "bolder" }}> <i class="fa-brands fa-strava" style={{ color: "#036EFD" }}></i> &nbsp; Model Training</h1><br/><br/>
           
           <br />
             {/* SVM Model */}
           <form>
              <h1 style={{ fontSize: "25px", fontWeight: "bolder", color:"grey" }}><i class="fa-solid fa-gears" style={{color:"#036EFD", fontSize:"20px"}}></i> &nbsp;SVM Model</h1>
-            <div class="mb-3"> <label htmlFor="exampleInputPassword1">Enter numeric value</label>
-              <input type="text" class="form-control" id="c-SVM" name="c-SVM" placeholder='Enter numeric value' value={1} style={{borderRadius:"20px"}} />
+            <div class="mb-3"> 
+              <br />
+              <label htmlFor="KernelSVM">Enter Kernal value:</label>
+              <br />
+              <select name="KernelSVM" id="KernelSVM" style={{borderRadius:"20px", width:"20vh"}}>
+                <option value="rbf">rbf</option>
+              </select>
             </div>
-            <input type="text" class="form-control" id="KernelSVM" name="KernelSVM" placeholder="Enter String Value"  style={{borderRadius:"20px"}}/>
+            <label htmlFor="cValSVM">Enter C value:</label>
+            <input type="text" class="form-control" id="cValSVM" name="cValSVM" placeholder=" Enter value between 10^-3 to 10^3"  style={{borderRadius:"20px"}}/>
+            <br />
+            <label htmlFor="gammaValSVM">Enter Gamma value:</label>
+            <input type="text" class="form-control" id="gammaValSVM" name="gammaValSVM" placeholder=" Enter value between 10^-3 to 10^3"  style={{borderRadius:"20px"}}/>
              <br />
             <button type="submit" className="btn btn-primary" style={{ borderRadius: "20px",width:"40vh" ,background: "radial-gradient(circle, rgba(157,86,224,1) 0%, rgba(253,130,85,1) 100%)", color: "white", borderColor: "#EFF2FF", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", }}>
             <i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Run SVM Model
@@ -70,22 +79,21 @@ const RunModels = () => {
             <br />
             <div class="mb-3">
               
-             <input type="text" class="form-control" id="nestimatorsRF" name="nestimatorsRF" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
-             <br />
-             <input type="text" class="form-control" id="maxdepthRF" name="maxdepthRF" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
-            </div>
-            <div class="dropdown">
-              <select name="minSampleSplitRF" id="minSampleSplitRF" style={{borderRadius:"20px", width:"20vh"}}>
-                <option value="0.2">2</option>
-                <option value="0.2">3</option>
-                <option value="0.2">4</option>
-                <option value="0.2">5</option>
-                <option value="0.2">6</option>
-              </select>
-            </div>
+            <label htmlFor="nestimatorsRF">Enter N Estimators value:</label>
+            <input type="text" class="form-control" id="nestimatorsRF" name="nestimatorsRF" placeholder=" Enter value between 1 to infinity"  style={{borderRadius:"20px"}}/>
+            
             <br />
+
+            <label htmlFor="MaxDepthDtree">Enter Max Depth value:</label>
+            <input type="text" class="form-control" id="MaxDepthDtree" name="MaxDepthDtree" placeholder=" Enter value between 1 to infinity or none"  style={{borderRadius:"20px"}}/>
+            
+            <br />
+
+            <label htmlFor="minSampleSplit">Enter Min Sample-Split value:</label>
+            <input type="text" class="form-control" id="minSampleSplit" name="minSampleSplit" placeholder=" Enter value between 2 to infinity"  style={{borderRadius:"20px"}}/>
+            </div>
             <button type="submit" className="btn btn-primary" style={{ borderRadius: "20px",width:"40vh" ,background: "radial-gradient(circle, rgba(157,86,224,1) 0%, rgba(253,130,85,1) 100%)", color: "white", borderColor: "#EFF2FF", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", }}>
-             <i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Run Random Forest
+             <i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Run Random Forest Model
             </button>
           </form>
           <br />
@@ -97,11 +105,18 @@ const RunModels = () => {
             <br />
             <div class="mb-3">
               
-             <input type="text" class="form-control" id="nestimatorXGB" name="nestimatorXGB" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
-             <br />
-             <input type="text" class="form-control" id="LearningRateXGB" name="LearningRateXGB" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
-             <br />
-             <input type="text" class="form-control" id="MaxDepthXGB" name="MaxDepthXGB" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
+            <label htmlFor="NEstimatorsXG">Enter N-Estimators value:</label>
+            <input type="text" class="form-control" id="NEstimatorsXG" name="NEstimatorsXG" placeholder=" Enter value between 0 to infinity"  style={{borderRadius:"20px"}}/>
+
+            <br />
+
+            <label htmlFor="MaxDepthXG">Enter Max Depth value:</label>
+            <input type="text" class="form-control" id="MaxDepthXG" name="MaxDepthXG" placeholder=" Enter value between 3 to infinity"  style={{borderRadius:"20px"}}/>
+
+            <br />
+
+            <label htmlFor="LearnRateXG">Enter Learning Rate value:</label>
+            <input type="text" class="form-control" id="LearnRateXG" name="LearnRateXG" placeholder=" Enter value between 0.01 to 0.3"  style={{borderRadius:"20px"}}/>
              <br />
             </div>
             <button type="submit" className="btn btn-primary" style={{ borderRadius: "20px",width:"40vh" ,background: "radial-gradient(circle, rgba(157,86,224,1) 0%, rgba(253,130,85,1) 100%)", color: "white", borderColor: "#EFF2FF", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", }}>
@@ -115,12 +130,26 @@ const RunModels = () => {
           <form>
              <h1 style={{ fontSize: "25px", fontWeight: "bolder", color:"grey" }}><i class="fa-solid fa-wand-magic-sparkles" style={{color:"#036EFD", fontSize:"20px"}}></i> &nbsp;Decision Tree </h1>
             <br />
-             <input type="text" class="form-control" id="CriterionDtree" name="CriterionDtree" placeholder="Enter String Value"  style={{borderRadius:"20px"}}/>
-             <br />
-             <input type="text" class="form-control" id="MaxDepthDtree" name="MaxDepthDtree" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
+            <br />
+            <label htmlFor="MaxDepthDtree">Enter Max Depth value:</label>
+            <input type="text" class="form-control" id="MaxDepthDtree" name="MaxDepthDtree" placeholder=" Enter value between 1 to infinity"  style={{borderRadius:"20px"}}/>
+            
+            <br />
+            <label htmlFor="minSampleSplitDtree">Enter Min Sample-Split value:</label>
+            <input type="text" class="form-control" id="minSampleSplitDtree" name="minSampleSplitDtree" placeholder=" Enter value between 2 to infinity"  style={{borderRadius:"20px"}}/>
+
+            <br />
+              <label htmlFor="KernelSVM">Enter Kernal value:</label>
+              <br />
+              <select name="KernelSVM" id="KernelSVM" style={{borderRadius:"20px", width:"20vh"}}>
+                <option value="gini">gini</option>
+                <option value="entropy">entropy</option>
+                <option value="log-loss">log-loss</option>
+              </select>
+              <br />
              <br />
             <button type="submit" className="btn btn-primary" style={{ borderRadius: "20px",width:"40vh" ,background: "radial-gradient(circle, rgba(157,86,224,1) 0%, rgba(253,130,85,1) 100%)", color: "white", borderColor: "#EFF2FF", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", }}>
-             <i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Run Decision Tree
+             <i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Run Decision Tree Model
             </button>
           </form>
           <br />
@@ -129,9 +158,21 @@ const RunModels = () => {
           <form>  <h1 style={{ fontSize: "25px", fontWeight: "bolder", color:"grey" }}><i class="fa-solid fa-bolt" style={{color:"#036EFD", fontSize:"20px"}}></i> &nbsp;Bagging </h1>
             <div class="mb-3">
             <br />
-             <input type="text" class="form-control" id="CriterionBaging" name="CriterionBaging" placeholder="Enter String Value"  style={{borderRadius:"20px"}}/>
-             <br />
-             <input type="text" class="form-control" id="MaxDepthDtree" name="MaxDepthDtree" placeholder="Enter Numeric Value"  style={{borderRadius:"20px"}}/>
+            <label htmlFor="NEstimatorsBagging">Enter N-Estimators value:</label>
+            <input type="text" class="form-control" id="NEstimatorsBagging" name="NEstimatorsBagging" placeholder=" Enter value between 1 to infinity"  style={{borderRadius:"20px"}}/>
+            
+            <br />
+
+            <label htmlFor="MaxSampleBagging">Enter Max Sample value:</label>
+            <input type="text" class="form-control" id="MaxSampleBagging" name="MaxSampleBagging" placeholder=" Enter value between 1 to infinity"  style={{borderRadius:"20px"}}/>
+            
+            <br />
+
+            <label htmlFor="MaxFeaturesBagging">Enter Max Feature value:</label>
+            <input type="text" class="form-control" id="MaxFeaturesBagging" name="MaxFeaturesBagging" placeholder=" Enter value between 0 to 1"  style={{borderRadius:"20px"}}/>
+
+            <br />
+
              <br />
             </div>
             <button type="submit" className="btn btn-primary" style={{ borderRadius: "20px",width:"40vh" ,background: "radial-gradient(circle, rgba(157,86,224,1) 0%, rgba(253,130,85,1) 100%)", color: "white", borderColor: "#EFF2FF", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", }}>
@@ -141,18 +182,6 @@ const RunModels = () => {
           <br />
           <br />
 
-          {/* Tpot */}
-          <form>  <h1 style={{ fontSize: "25px", fontWeight: "bolder", color:"grey" }}><i class="fa-solid fa-fire" style={{color:"#036EFD", fontSize:"20px"}}></i> &nbsp;Tpot autoML</h1>
-          <br />
-            <div class="mb-3">
-             <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter String Value"  style={{borderRadius:"20px"}}/>
-            </div>
-            <button type="submit" className="btn btn-primary" style={{ borderRadius: "20px",width:"40vh" ,background: "radial-gradient(circle, rgba(157,86,224,1) 0%, rgba(253,130,85,1) 100%)", color: "white", borderColor: "#EFF2FF", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", }}>
-             <i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; &nbsp; Normalize date column
-            </button>
-            </form>
-          <br />
-          <br />
           
         </div>
 
@@ -178,7 +207,7 @@ const RunModels = () => {
             }}
           >
            
-           <h1 style={{ fontSize: "30px", fontWeight: "bolder" }}> <i className="fa-solid fa-file-csv" style={{ color: "#036EFD" }}></i> &nbsp; Uploaded Dataset</h1><br/>
+           <h1 style={{ fontSize: "30px", fontWeight: "bolder" }}> <i className="fa-solid fa-file-csv" style={{ color: "#036EFD" }}></i> &nbsp; Ouput Dataset</h1><br/>
           </div>
           <br />
           <table
